4ffbe5b4f7319ca2be362b7b9eb0b998398a6df4
Brent <riva.brent@gmail.com>
2019-10-20 13:49:34 -0400

    read spooky secrets from file

--- truffle-config.js
+++ truffle-config.js
@@ -1,25 +1,39 @@
 const HDWalletProvider = require('truffle-hdwallet-provider')
+const fs = require('fs');
+let secrets;
 
+if (fs.existsSync('.secret')) {
+ secrets = JSON.parse(fs.readFileSync('.secret', 'utf8'));
+}
 module.exports = {
   networks: {
-    cldev: {
+    dev: {
       host: '127.0.0.1',
-      port: 8545,
+      port: 7545,
       network_id: '*',
     },
-    live: {
+    ropsten: {
       provider: () => {
-        return new HDWalletProvider(process.env.MNEMONIC, process.env.RPC_URL)
+        return new HDWalletProvider(secrets.mnemonic, 'https://ropsten.infura.io/v3/' + secrets.infuraApiKey)
       },
-      network_id: '*',
+      network_id: '3',
       // Necessary due to https://github.com/trufflesuite/truffle/issues/1971
       // Should be fixed in Truffle 5.0.17
       skipDryRun: true,
     },
+    mainnet: {
+      provider: () => {
+        return new HDWalletProvider(secrets.mnemonic, 'https://mainnet.infura.io/v3/' + secrets.infuraApiKey)
+      },
+      network_id: '1',
+      // Necessary due to https://github.com/trufflesuite/truffle/issues/1971
+      // Should be fixed in Truffle 5.0.17
+      skipDryRun: true,
+    }
   },
   compilers: {
     solc: {
-      version: '0.4.24',
+      version: '0.5.8',
     },
   },
 }
