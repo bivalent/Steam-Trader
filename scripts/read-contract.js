802c09d239203b29756a9e8a37e9282a5ded6da4
Brent <riva.brent@gmail.com>
2019-10-29 23:05:27 -0400

    fix scripts/ and increase default payment amount to 5 stinkies

--- scripts/read-contract.js
+++ scripts/read-contract.js
@@ -1,4 +1,4 @@
-const MyContract = artifacts.require('MyContract')
+const SteamTrader = artifacts.require('SteamTrader')
 
 /*
   This script makes it easy to read the data variable
@@ -6,7 +6,7 @@ const MyContract = artifacts.require('MyContract')
 */
 
 module.exports = async callback => {
-  const mc = await MyContract.deployed()
-  const data = await mc.data.call()
+  const st = await SteamTrader.deployed()
+  const data = await st.data.call()
   callback(data)
 }
--- scripts/request-data.js
+++ scripts/request-data.js
@@ -1,33 +1,50 @@
-const MyContract = artifacts.require('MyContract')
+const SteamTrader = artifacts.require('SteamTrader')
+const uuidv4 = require('uuid/v4')
 
 /*
   This script allows for a Chainlink request to be created from
-  the requesting contract. Defaults to the Chainlink oracle address
-  on this page: https://docs.chain.link/docs/testnet-oracles
+  the requesting contract. Defaults to my ropsten oracle address
 */
 
 const oracleAddress =
   process.env.TRUFFLE_CL_BOX_ORACLE_ADDRESS ||
-  '0xc99B3D447826532722E41bc36e644ba3479E4365'
+  '0x815286136dce0009082c0c1397647583cdc03d95'
 const jobId =
-  process.env.TRUFFLE_CL_BOX_JOB_ID || '3cff0a3524694ff8834bda9cf9c779a1'
+  process.env.TRUFFLE_CL_BOX_JOB_ID || '93c4b78e3aa64c2a895473d2e834edb7'
 const payment = process.env.TRUFFLE_CL_BOX_PAYMENT || '1000000000000000000'
-const url =
-  process.env.TRUFFLE_CL_BOX_URL ||
-  'https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD'
-const path = process.env.TRUFFLE_CL_BOX_JSON_PATH || 'USD'
-const times = process.env.TRUFFLE_CL_BOX_TIMES || '100'
+
+const user_id = '76561197993433424'
+const appid = 570
+const context = 2
+const assetid = 6908576449
+const classid = 948149724
+const instanceid = 996698943
+
+/*
+bytes32 _uuid,
+string calldata _steamId,
+uint256 _askingPrice,
+// item
+uint64 _assetid,
+uint64 _classid,
+uint64 _instanceid,
+// app
+uint16 _appId,
+uint8 _inventoryContext
+*/
 
 module.exports = async callback => {
-  const mc = await MyContract.deployed()
+  const st = await SteamTrader.deployed()
   console.log('Creating request on contract:', mc.address)
-  const tx = await mc.createRequestTo(
-    oracleAddress,
-    web3.utils.toHex(jobId),
+  const tradeId = await st.createTrade(
+    uuidv4().replace(/-/g, ''),
+    user_id,
     payment,
-    url,
-    path,
-    times,
+    assetid,
+    classid,
+    instanceid,
+    appid,
+    context
   )
   callback(tx.tx)
 }
