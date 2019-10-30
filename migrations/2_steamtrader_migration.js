3ed84c949b737b39d942556358644f1afc792a62
Brent <riva.brent@gmail.com>
2019-10-29 23:05:03 -0400

    rename migration file accordingly

--- migrations/2_steamtrader_migration.js
+++ migrations/2_steamtrader_migration.js
@@ -3,7 +3,7 @@ let MyContract = artifacts.require('SteamTrader')
 module.exports = (deployer, network) => {
   // Local (development) networks need their own deployment of the LINK
   // token and the Oracle contract
-  if (!network.startsWith('live')) {
+  if (!network == 'ropsten' && !network == 'mainnet') {
     // Being lazy, do nothing...
   } else {
     // For live networks, use the 0 address to allow the ChainlinkRegistry
