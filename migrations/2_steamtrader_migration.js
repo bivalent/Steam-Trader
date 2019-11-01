69955de29af3e9f73e9b7323831c6baf51dbf734
Brent <riva.brent@gmail.com>
2019-10-31 22:33:29 -0400

    working ropsten migration

--- migrations/2_steamtrader_migration.js
+++ migrations/2_steamtrader_migration.js
@@ -1,10 +1,16 @@
-let MyContract = artifacts.require('SteamTrader')
+let SteamTrader = artifacts.require('SteamTrader')
+let LinkToken = artifacts.require('chainlink/v0.5/contracts/interfaces/ChainlinkRequestInterface.sol')
+let Oracle = artifacts.require('Oracle')
 
 module.exports = (deployer, network) => {
   // Local (development) networks need their own deployment of the LINK
   // token and the Oracle contract
-  if (!network == 'ropsten' && !network == 'mainnet') {
-    // Being lazy, do nothing...
+  if (!network.startsWith('ropsten') && !network.startsWith('mainnet')) {
+    deployer.deploy(LinkToken).then(() => {
+      return deployer.deploy(Oracle, LinkToken.address).then(() => {
+        return deployer.deploy(SteamTrader, LinkToken.address)
+      })
+    })
   } else {
     // For live networks, use the 0 address to allow the ChainlinkRegistry
     // contract automatically retrieve the correct address for you
--- truffle-config.js
+++ truffle-config.js
@@ -3,22 +3,23 @@ const HDWalletProvider = require('truffle-hdwallet-provider')
 const fs = require('fs')
 let secrets
 
-if (fs.existsSync('.secret')) {
-  secrets = JSON.parse(fs.readFileSync('.secret', 'utf8'))
+if (fs.existsSync('.s/.secret')) {
+  secrets = JSON.parse(fs.readFileSync('.s/.secret', 'utf8'))
 }
+
 module.exports = {
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   networks: {
-    dev: {
+    develop: {
       host: '127.0.0.1',
-      port: 7545,
-      network_id: '*',
+      port: 9545,
+      network_id: '*'
     },
     ropsten: {
       provider: () => {
         return new HDWalletProvider(
           secrets.mnemonic,
-          'https://ropsten.infura.io/v3/' + secrets.infuraApiKey,
+          'https://ropsten.infura.io/v3/' + secrets.infuraKey,
         )
       },
       network_id: '3',
@@ -27,7 +28,7 @@ module.exports = {
       provider: () => {
         return new HDWalletProvider(
           secrets.mnemonic,
-          'https://mainnet.infura.io/v3/' + secrets.infuraApiKey,
+          'https://mainnet.infura.io/v3/' + secrets.infuraKey,
         )
       },
       network_id: '1',
